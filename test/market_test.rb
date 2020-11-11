95d6713262d8ded3fe9a0337ab64e63b240752b5
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:59:06 -0700

    add sorted test

--- test/market_test.rb
+++ test/market_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/market'
 require './lib/item'
 require './lib/vendor'
-
 # Iteration 2
 class MarketTest < Minitest::Test
   def setup
@@ -121,5 +120,22 @@ class MarketTest < Minitest::Test
     @market.add_vendor(@vendor3)
 
     assert_equal [@item1], @market.overstocked_items
+  end
+
+  def test_it_can_list_sorted_items
+  # Add a method to your `Market` class called `sorted_item_list` that returns a
+  # list of names of all items the Vendors have in stock, sorted alphabetically.
+  # This list should not include any duplicate items.
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = ['Banana Nice Cream', 'Peach', 'Peach-Raspberry Nice Cream', 'Tomato']
+    assert_equal expected, @market.sorted_item_list
   end
 end
