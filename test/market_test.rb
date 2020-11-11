cf48a170f1a8c2ff97faf72e74340f4177c7c01e
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:31:33 -0700

    add add vendor test

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,6 +9,8 @@ class MarketTest < Minitest::Test
   def setup
     @market = Market.new('South Pearl Street Farmers Market')
     @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new('Ba-Nom-a-Nom')
+    @vendor3 = Vendor.new('Palisade Peach Shack')
     @item1 = Item.new({name: 'Peach', price: '$0.75'})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
@@ -22,5 +24,19 @@ class MarketTest < Minitest::Test
 
   def test_it_starts_with_no_vendors
     assert_equal [], @market.vendors
+  end
+
+    def test_it_can_add_vendors
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 end
