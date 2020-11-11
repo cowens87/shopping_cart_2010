90cf4bf47107027c5682d554b35e6f424569ad45
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:23:03 -0700

    add stock method

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -37,13 +37,9 @@ class VendorTest < Minitest::Test
 
     assert_equal 55, @vendor.check_stock(@item1)
 
-
     @vendor.stock(@item2, 12)
 
-    expected = {
-                @item1 => 55,
-                @item2 => 12
-              }
+    expected = {@item1 => 55, @item2 => 12}
     assert_equal expected, @vendor.inventory
   end
 end
