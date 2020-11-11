5744c32d75fe613bbb169ada427eafa700734459
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:42:29 -0700

    add potential revenue method

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -13,4 +13,10 @@ class Vendor
   def stock(item, quantity)
     @inventory[item] += quantity
   end
+
+  def potential_revenue
+    @inventory.sum do |item, qty|
+      (item.price * qty)
+    end
+  end
 end
