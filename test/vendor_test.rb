422733017406a95f1be5dc9658825b535e5f7aaa
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:21:57 -0700

    add stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -24,4 +24,26 @@ class VendorTest < Minitest::Test
   def test_it_can_check_stock
     assert_equal 0, @vendor.check_stock(@item1)
   end
+
+  def test_it_can_stock_items
+    @vendor.stock(@item1, 30)
+
+    expected = {@item1 => 30}
+    assert_equal expected, @vendor.inventory
+
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+
+    assert_equal 55, @vendor.check_stock(@item1)
+
+
+    @vendor.stock(@item2, 12)
+
+    expected = {
+                @item1 => 55,
+                @item2 => 12
+              }
+    assert_equal expected, @vendor.inventory
+  end
 end
