1ee128940f4d53def2d63cbb25b5ebee0af947f1
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:41:56 -0700

    add potential revenue test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -7,8 +7,13 @@ class VendorTest < Minitest::Test
   # Iteration 1
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new('Ba-Nom-a-Nom')
+    @vendor3 = Vendor.new('Palisade Peach Shack')
     @item1 = Item.new({name: 'Peach', price: '$0.75'})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    @item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
   end 
 
   def test_it_exists_and_has_attributes
@@ -41,5 +46,20 @@ class VendorTest < Minitest::Test
 
     expected = {@item1 => 55, @item2 => 12}
     assert_equal expected, @vendor.inventory
+  end
+
+  # End of Iteration 2
+  def test_it_can_find_potential_revenue
+  # A Vendor will be able to calculate their `potential_revenue` -
+  # the sum of all their items' price * quantity.
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    assert_equal 29.75, @vendor1.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75, @vendor3.potential_revenue
   end
 end
