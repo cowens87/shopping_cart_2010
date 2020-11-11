e00bb067ced7ab929c77f6cf1c1c7fcbfc1d3e2e
Cydnee Owens <cks410@yahoo.com>
2020-11-11 09:49:12 -0700

    add total inventory test

--- test/market_test.rb
+++ test/market_test.rb
@@ -64,4 +64,30 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
   end
+
+  # Iteration 3
+  def test_it_list_total_inventory
+  # Additionally, your `Market` class should have a method called `total_inventory`
+  # that reports the quantities of all items sold at the market. Specifically,
+  # it should return a hash with items as keys and hash as values -
+  # this sub-hash should have two key/value pairs: quantity pointing to total
+  # inventory for that item and vendors pointing to an array of the vendors that
+  # sell that item.
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected =  {
+                @item1 => {quantity: 100, vendors: [@vendor1, @vendor3]},
+                @item2 => {quantity: 7, vendors: [@vendor1]},
+                @item4 => {quantity: 50, vendors: [@vendor2]},
+                @item3 => {quantity: 35, vendors: [@vendor2, @vendor3]},
+                }
+    assert_equal expected, @market.total_inventory
+  end
 end
